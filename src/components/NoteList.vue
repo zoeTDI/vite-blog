5026782c8420c9ac2167518b2e0d9c908039d618
Caldm <1832400547@qq.com>
2024-12-01 12:48:21 +0800

    feat(NoteList): 从 API 获取笔记数据并优化笔记列表显示- 通过 axios 从 http://localhost:8080/api/getNotes@latest 获取笔记数据 - 在组件挂载时调用 fetchNotes 函数获取数据 -优化笔记列表显示，使用 summary 字段替代 content 字段 -保留按类别过滤笔记的功能

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,18 +1,36 @@
 <script setup>
 
 import NoteCard from "@/components/NoteCard.vue";
-import data from "../../public/data.json";
+// import data from "../../public/data.json";
 import {useRoute} from "vue-router";
-import {ref, watch} from "vue";
-const noteData = ref(data);
+import {onMounted, ref, watch} from "vue";
+import axios from "axios";
+
+const noteData = ref([]);
 const route = useRoute();
+
+// data 的数据该从 http://localhost:8080/api/getNotes@latest 初获取
+async function fetchNotes() {
+  const res = await axios.get("http://localhost:8080/api/getNotes@latest", {
+    params: {
+      size: 10,
+      page: 1
+    }
+  });
+  noteData.value = res.data;
+  console.log(noteData.value);
+}
+
+onMounted(() => {
+  fetchNotes();
+})
 watch(noteData, (newNoteData) => {
   noteData.value = newNoteData;
 })
 watch(() => route.params.cate, (newCate) => {
   if (newCate === 'all') {
     noteData.value = data;
-  }else {
+  } else {
     noteData.value = data.filter(note => note.category === newCate);
   }
 })
@@ -25,7 +43,7 @@ watch(() => route.params.cate, (newCate) => {
         :key="index"
         :pic="note.pic"
         :title="note.title"
-        :summary="note.content.split('\n\n').slice(0, 1).join('\n\n')"
+        :summary="note.summary.split('\n\n').slice(0, 1).join('\n\n')"
         :date="note.date"
         :tags="note.tags"
         :link="{ name: 'Note', params: { id: note.id } }"
