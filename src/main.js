b580558746cdf87a284ccb285bf57f117a1568a7
Caldm <1832400547@qq.com>
2024-12-01 14:30:00 +0800

    feat(components): 优化记事卡片组件的图片显示
    - 添加 useAdjustImageSize 钩子，用于调整图片大小
    - 在 NoteCard 组件中集成该钩子，实现图片自适应显示
    - 优化图片样式，设置 object-fit: cover 以保持图片比例- 调整内容区域高度，确保布局一致性

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import vSlideIn from "@/js/vSlideIn.js";
 
-const app = createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.directive('side-in', vSlideIn);
+app.use(router);
+app.mount("#app");
 
