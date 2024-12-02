c1d84439f249f2934f5dfc2b8627f9cc41a62323
Caldm <1832400547@qq.com>
2024-12-02 15:10:51 +0800

    feat(SideIn): 添加页面侧边淡入效果
    - 新增 IntersectionObserver 用于检测元素是否进入视口
    - 为视口外的元素添加淡入动画效果
    - 优化了元素加载和显示的性能

--- src/js/SideIn.js
+++ src/js/SideIn.js
@@ -1,6 +1,41 @@
+const TRANSITION_DURATION = 80;
+const DURATION = 300;
+const map = new WeakMap();
 export default {
     mounted(el) {
+        function isBelowViewport(el) {
+            return el.getBoundingClientRect().top > window.innerHeight;
+        }
 
+        const ob = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    map.get(entry.target).play();
+                    ob.unobserve(entry.target);
+                }
+            })
+        })
+        if (isBelowViewport(el)) {
+            const animation = el.animate(
+                [
+                    {
+                        transform: "translateY(" + TRANSITION_DURATION + "px)",
+                        opacity: 0.5
+                    },
+                    {
+                        transform: "translateY(0)",
+                        opacity: 1
+                    }
+                ],
+                {
+                    duration: DURATION,
+                    easing: "ease-out",
+                    fill: "forwards"
+                })
+            animation.pause();
+            ob.observe(el);
+            map.set(el, animation);
+        }
     },
     unmounted(el) {
 
